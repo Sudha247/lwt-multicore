159e63c55fab0df4edf23233124ea84bdbf2e469
Sudha Parimala <sudharg247@gmail.com>
2020-09-28 11:34:18 +0530

    locks used only internally

--- src/core/lwt_lock.mli
+++
@@ -1,9 +0,0 @@
-(** Locks for m*)
-
-type t
-val create_lock : unit -> t 
-val acquire_lock : t -> unit 
-val release_lock : t -> unit 
-val is_locked : t -> bool
-val try_acquire : t -> bool
-val try_release : t -> bool
